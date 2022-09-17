c73c01956bc8a1ede6931ebfc67351a40334e910
derfelot <derfelot.git@gmail.com>
2022-09-18 00:11:53 +0200

    yoshino-common: config: Drop custom APN's
    Shouldn't be needed anymore with new blobs.

--- platform/config-files.mk
+++ platform/config-files.mk
@@ -78,10 +78,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
-### TELEPHONY
-PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/config/telephony/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
-
 # IMS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
